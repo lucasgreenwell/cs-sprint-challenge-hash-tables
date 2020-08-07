f2add4a4a41885cedf9a41d2e102a1943d0ddf9a
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-08-07 10:56:27 -0600

    finishes pathfinder < lol

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -6,9 +6,45 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    #loop through the list of paths
+        #grab the filename from the end of the path
+        #if it's in the cache already then just append the path to the list of paths
+        #if it's not, then add it
+    #loop through the list of queries
+        #append the value of the query in the cache to the result
+    possible_queries = {}
+    result = []
 
+    for path in files:
+        partial_paths = path.split("/")
+        possible_query = partial_paths[-1]
+
+        if possible_query in possible_queries:
+            possible_queries[possible_query].append(path)
+        else:
+            possible_queries[possible_query] = [path]
+
+    for query in queries:
+        if query in possible_queries:
+            for item in possible_queries[query]:
+                result.append(item)
+
+    print(result)
     return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
